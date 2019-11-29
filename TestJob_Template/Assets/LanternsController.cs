de868d81835f031a15b3dcc4a06dd7b0f10cc5aa
Banana090 <spy090@yandex.ru>
2019-11-29 18:29:08 +0300

    Task 1 DONE

---
+++ TestJob_Template/Assets/LanternsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternsController : MonoBehaviour
+{
+    [SerializeField] private Animator[] lanternAnimators;
+    [SerializeField] private bool randomizeAnimOffset;
+
+    private bool isWind;
+
+    private void Start()
+    {
+        if (randomizeAnimOffset)
+        {
+            foreach (Animator anim in lanternAnimators)
+                anim.SetFloat("animOffset", Random.value);
+        }
+    }
+
+    public void ToggleWind()
+    {
+        isWind = !isWind;
+
+        foreach (Animator anim in lanternAnimators)
+            anim.SetBool("isWind", isWind);
+    }
+}
